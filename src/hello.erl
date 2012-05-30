efeb96c2f813dbaf098332034edbf7c46b4e04d9
Dave Cottlehuber <dch@apache.org>
2012-05-30 13:15:26 +0200

    Use improved NIF loader c/ @davisp

--- src/hello.erl
+++ src/hello.erl
@@ -2,8 +2,26 @@
 
 -export([init/0, world/0]).
 
-init() ->
-      erlang:load_nif("./priv/hello", 0).
-
 world() ->
       "NIF library not loaded".
+
+init() ->
+    SoName = case code:priv_dir(?MODULE) of
+    {error, bad_name} ->
+        case filelib:is_dir(filename:join(["..", priv])) of
+        true ->
+            filename:join(["..", priv, ?MODULE]);
+        false ->
+            filename:join([priv, ?MODULE])
+        end;
+    Dir ->
+        filename:join(Dir, ?MODULE)
+    end,
+    (catch erlang:load_nif(SoName, 0)),
+    case erlang:system_info(otp_release) of
+    "R13B03" -> true;
+    _ -> ok
+    end.
+
+
+
